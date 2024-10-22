41db4c6b2ee878fca6ccf31ec29dc11b076a150c
Balendra Pal <balendrapal615@gmail.com>
2024-10-22 13:24:52 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -1,13 +1,14 @@
 #Github.com/mrinvisible7
 
+import logging, time, sys
 from pyrogram import Client
 
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 from decouple import config
-import logging, time, sys
 
+#logger setup
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
@@ -27,12 +28,15 @@ if len(AUTH) != 0:
 else:
     SUDO_USERS = set()
 
+# Create Telethon bot
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
 #userbot = Client(
 #    session_name=SESSION, 
 #    api_hash=API_HASH, 
 #    api_id=API_ID)
+
+# Create Pyrogram userbot
 userbot = Client("myacc",api_id=API_ID,api_hash=API_HASH,session_string=SESSION)
 
 try:
@@ -41,6 +45,7 @@ except BaseException:
     print("Userbot Error ! Have you added SESSION while deploying??")
     sys.exit(1)
 
+# Create Pyrogram bot
 Bot = Client(
     "SaveRestricted",
     bot_token=BOT_TOKEN,
@@ -52,5 +57,5 @@ try:
     Bot.start()
 except Exception as e:
     #print(e)
-    logger.info(e)
+    logging.info(e)
     sys.exit(1)
