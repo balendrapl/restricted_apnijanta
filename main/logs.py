5cfc07640db31fd887a2443c0140be8c8d4d90fe
Balendra Pal <balendrapal615@gmail.com>
2024-10-22 22:21:10 +0530

    small changes

--- main/logs.py
+++ main/logs.py
@@ -13,5 +13,12 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+# Get the main logger
+logger = logging.getLogger()
 
-logging = logging.getLogger()
+# Example log messages
+# logger.debug("This is a debug message")
+# logger.info("This is an info message")
+# logger.warning("This is a warning message")
+# logger.error("This is an error message")
+# logger.critical("This is a critical message")
