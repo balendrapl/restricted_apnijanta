153356cf3de8cf0043a16210f76c7e8ba3ec8e53
Balendra Pal <balendrapal615@gmail.com>
2024-10-22 22:19:05 +0530

    some small changes

--- main/vars.py
+++ main/vars.py
@@ -2,9 +2,13 @@
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-SESSION = os.environ.get("SESSION", "")
-FORCESUB = os.environ.get("FORCESUB", "")
-AUTH = os.environ.get("AUTH", "")
+API_ID    = os.environ.get("API_ID")
+API_HASH  = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+SESSION = os.environ.get("SESSION")
+FORCESUB = os.environ.get("FORCESUB")
+AUTH = os.environ.get("AUTH")
+
+# Raise an error if any critical environment variable is missing
+# if not API_ID or not API_HASH or not BOT_TOKEN:
+#     raise ValueError("API_ID, API_HASH, and BOT_TOKEN must be set as environment variables")
